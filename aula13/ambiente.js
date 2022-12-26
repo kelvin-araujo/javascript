4f0df28218129a8a5b07dacd286f659544c828a6
Kelvin Araújo <kelvinaraujoh@gmail.com>
2022-12-26 14:31:38 -0300

    Initial commit

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+let c = 1
+while( c <= 6 ) {
+    console.log('Tudo bem?')
+    c++
+}
+
+
+/*let c = 1
+do {
+    console.log('Tudo bem?')
+    c++
+} while ( c <= 6 ) 
+                Nesse código usamos a estrutura do while,
+                que é uma estrutura de repetição com teste lógico no final
+
+*/
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
